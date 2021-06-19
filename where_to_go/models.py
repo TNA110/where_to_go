abb131a4da5dc4ba755fa2e47ce36325c134662f
TNA110 <73946838+TNA110@users.noreply.github.com>
2021-06-20 00:25:41 +0300

    Добавил русскоязычные названия полей моделей

--- where_to_go/models.py
+++ where_to_go/models.py
@@ -3,21 +3,21 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.TextField()
-    description_long = HTMLField()
-    longtitude = models.FloatField()
-    latitude = models.FloatField()
+    title = models.CharField(max_length=200, verbose_name="Название")
+    description_short = models.TextField(verbose_name="Краткое описание")
+    description_long = HTMLField(verbose_name="Полное описание")
+    longtitude = models.FloatField(verbose_name="Долгота")
+    latitude = models.FloatField(verbose_name="Широта")
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(blank=True)
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, blank=False, editable=True)
-    order = models.PositiveIntegerField(default=0, blank=False, null=False, editable=True)
+    title = models.CharField(max_length=200, verbose_name="Название")
+    image = models.ImageField(blank=True, verbose_name="Изображение")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, blank=False, editable=True, verbose_name="Локация")
+    order = models.PositiveIntegerField(default=0, blank=False, null=False, editable=True, verbose_name="Порядковый номер")
 
     class Meta(object):
         ordering = ['order', ]
